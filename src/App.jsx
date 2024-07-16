998cb6a42eca7f4ec6b7481b054a6c9b0b9f8be6
Mayank Sethi <mayank@digibrain.co.in>
2024-07-16 15:53:21 +0530

    delete confirmation cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -74,10 +74,10 @@ function App() {
   return (
     <>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
+          <DeleteConfirmation
+            onCancel={handleStopRemovePlace}
+            onConfirm={handleRemovePlace}
+          />
       </Modal>
 
       <header>
